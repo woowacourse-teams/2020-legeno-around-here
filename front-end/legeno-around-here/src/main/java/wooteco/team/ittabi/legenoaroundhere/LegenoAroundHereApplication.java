f2d8d1d4ae5841618205c5480f7fb5f0d5f2fc68
hamliet <hamliet@hanyang.ac.kr>
2020-07-10 18:30:59 +0900

    feat: 프로젝트 생성 [ISSUE-26]

---
+++ front-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/LegenoAroundHereApplication.java
@@ -0,0 +1,13 @@
+package wooteco.team.ittabi.legenoaroundhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegenoAroundHereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegenoAroundHereApplication.class, args);
+    }
+
+}
