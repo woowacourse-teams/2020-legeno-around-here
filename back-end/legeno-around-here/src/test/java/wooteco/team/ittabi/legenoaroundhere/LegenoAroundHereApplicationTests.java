8ef5782fe7c921c5660380af5f479d1e482a685c
Hamliet <hamliet@hanyang.ac.kr>
2020-07-13 00:37:08 +0900

    feat: 백엔드 프로젝트 생성 [ISSUE-26] (#27)
    * feat: 프로젝트 생성 [ISSUE-26]
    
    * fix: 백엔드 폴더 이름 수정

---
+++ back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/LegenoAroundHereApplicationTests.java
@@ -0,0 +1,13 @@
+package wooteco.team.ittabi.legenoaroundhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegenoAroundHereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
