22a091f8b5406266f3db30fd95d193d1e0e53039
junk-sound <wom2277@naver.com>
2020-07-20 16:46:02 +0900

    feat: LegalArea 생성자 추가

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/area/LegalArea.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/area/LegalArea.java
@@ -1,10 +1,17 @@
 package wooteco.team.ittabi.legenoaroundhere.domain.area;
 
+import java.util.List;
+
 class LegalArea extends Area {
 
     LegalArea(Regions regions) {
         super(regions);
     }
+
+    LegalArea(List<Region> regions) {
+        super(new Regions(regions));
+    }
+
 
     @Override
     Region findSmallestRegion() {
