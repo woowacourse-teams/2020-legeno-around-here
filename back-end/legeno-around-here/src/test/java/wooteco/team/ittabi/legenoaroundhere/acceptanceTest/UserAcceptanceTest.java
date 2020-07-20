16ac92439add6dbbe334a281109c816d0d61144c
hamliet <hamliet@hanyang.ac.kr>
2020-07-20 16:02:34 +0900

    feat: 회원 인수 테스트 작성 및 TokenResponse DTO 구현

---
+++ back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/acceptanceTest/UserAcceptanceTest.java
@@ -0,0 +1,91 @@
+package wooteco.team.ittabi.legenoaroundhere.acceptanceTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import wooteco.team.ittabi.legenoaroundhere.dto.TokenResponse;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserAcceptanceTest {
+
+    private static final String TEST_EMAIL = "test@test.com";
+    private static final String TEST_NAME = "testName";
+    private static final String TEST_PASSWORD = "test";
+    private static final String USER_LOCATION_PATTERN = "^/users/[1-9][0-9]*$";
+
+    @LocalServerPort
+    public int port;
+
+    public static RequestSpecification given() {
+        return RestAssured.given().log().all();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+    }
+
+    //        Feature: 회원관리
+    //
+    //        Scenario: 회원을 관리한다.
+    //        When 회원 가입 요청을 한다.
+    //        Then 회원으로 가입이되었다.
+    //
+    //        When 로그인을 한다.
+    //        Then 로그인이 되었다.
+    @Test
+    public void manageUser() {
+//        String location = createUser(TEST_EMAIL, TEST_NAME, TEST_PASSWORD);
+        String location = "/users/1";     // Todo: 지울것
+        assertThat(location).matches(USER_LOCATION_PATTERN);
+
+//        TokenResponse tokenResponse = login(TEST_EMAIL, TEST_PASSWORD);
+        TokenResponse tokenResponse = new TokenResponse("abc");   // Todo: 지울것
+        assertThat(tokenResponse.getAccessToken()).isNotNull();
+    }
+
+    private String createUser(String email, String name, String password) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("name", name);
+        params.put("password", password);
+
+        return
+            given().
+                body(params).
+                contentType(MediaType.APPLICATION_JSON_VALUE).
+                accept(MediaType.APPLICATION_JSON_VALUE).
+            when().
+                post("/join").
+            then().
+                statusCode(HttpStatus.CREATED.value()).
+                extract().header("Location");
+    }
+
+    private TokenResponse login(String email, String password) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+
+        return
+            given().
+                body(params).
+                contentType(MediaType.APPLICATION_JSON_VALUE).
+                accept(MediaType.APPLICATION_JSON_VALUE).
+            when().
+                post("/login").
+            then().
+                log().all().
+                statusCode(HttpStatus.OK.value()).
+                extract().as(TokenResponse.class);
+    }
+}
