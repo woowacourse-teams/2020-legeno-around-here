d7afe8bfa796ef0d16eaff8d206495db50ba0c1b
jamie9504 <jamie9504@kakao.com>
2020-07-21 13:41:37 +0900

    refactor: Enum 저장 방식 변경 (Ordinal > String)

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/Post.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/Post.java
@@ -2,6 +2,8 @@ package wooteco.team.ittabi.legenoaroundhere.domain;
 
 import java.util.Objects;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @Entity
 public class Post extends BaseEntity {
@@ -9,6 +11,7 @@ public class Post extends BaseEntity {
     private static final int LIMIT_LENGTH = 20;
 
     private String writing;
+    @Enumerated(EnumType.STRING)
     private State state;
 
     protected Post() {
