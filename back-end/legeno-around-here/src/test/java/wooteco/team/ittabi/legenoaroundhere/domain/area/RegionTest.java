c6871ab5cdd2730790858497aa990881a5c8808c
junk-sound <wom2277@naver.com>
2020-07-21 14:53:05 +0900

    refac: RegionTest에 ParameterizedTest 적용

--- back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/domain/area/RegionTest.java
+++ back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/domain/area/RegionTest.java
@@ -4,28 +4,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class RegionTest {
 
     @DisplayName("region 생성 성공")
-    @Test
-    void construct() {
-        Region regionLevelOne = new Region("서울시", RegionDepth.ONE);
+    @ParameterizedTest
+    @ValueSource(strings = {"서울시", "서대문구", "홍제동"})
+    void construct(String regionName) {
+        Region regionLevelOne = new Region(regionName, RegionDepth.ONE);
         assertThat(regionLevelOne).isNotNull();
-        Region regionLevelTwo = new Region("서대문구", RegionDepth.TWO);
-        assertThat(regionLevelTwo).isNotNull();
-        Region regionLevelThree = new Region("홍제동", RegionDepth.THREE);
-        assertThat(regionLevelThree).isNotNull();
     }
 
-    @DisplayName("비교 학습")
+    @DisplayName("비교 확인")
     @Test
     void compareTo() {
-        Region regionLevelOne = new Region("서울시", RegionDepth.ONE);
-        Region regionLevelTwo = new Region("서대문구", RegionDepth.TWO);
-        Region regionLevelTwoAnother = new Region("마포구", RegionDepth.TWO);
+        Region regionDepthOne = new Region("서울시", RegionDepth.ONE);
+        Region regionDepthTwo = new Region("서대문구", RegionDepth.TWO);
+        Region regionDepthTwoAnother = new Region("마포구", RegionDepth.TWO);
 
-        assertThat(regionLevelOne.compareTo(regionLevelTwo)).isEqualTo(-1);
-        assertThat(regionLevelTwo.compareTo(regionLevelTwoAnother)).isEqualTo(0);
+        assertThat(regionDepthOne.compareTo(regionDepthTwo)).isEqualTo(-1);
+        assertThat(regionDepthTwo.compareTo(regionDepthTwoAnother)).isEqualTo(0);
     }
 }
