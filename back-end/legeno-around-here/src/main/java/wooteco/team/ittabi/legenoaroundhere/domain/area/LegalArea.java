a6d81404918ee5790c4c1502e4810aee5758e614
junk-sound <wom2277@naver.com>
2020-07-24 12:32:11 +0900

    refac: 히히 피드백 반영

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/area/LegalArea.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/area/LegalArea.java
@@ -2,9 +2,9 @@ package wooteco.team.ittabi.legenoaroundhere.domain.area;
 
 import java.util.Map;
 
-class LegalArea extends Area {
+public class LegalArea extends Area {
 
-    protected LegalArea(
+    public LegalArea(
         Map<RegionDepth, Region> regions) {
         super(regions);
     }
