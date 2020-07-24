760d38fe3b6ddf6ee95f95da0cfb62c86675f7c0
junk-sound <wom2277@naver.com>
2020-07-24 12:33:38 +0900

    refac: 빙봉 피드백 반영

--- back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/domain/area/RegionNameTest.java
+++ back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/domain/area/RegionNameTest.java
@@ -12,7 +12,7 @@ class RegionNameTest {
 
     @DisplayName("1글자 이상, 10글자 이하의 글자를 활용한 RegionName 생성 - 성공")
     @Test
-    void construct_nameLengthUnder10OrOver1_Constructed() {
+    void construct_NameLengthUnder10OrOver1_Constructed() {
         RegionName regionName = new RegionName("홍제동");
         assertThat(regionName).isNotNull();
     }
@@ -20,7 +20,7 @@ class RegionNameTest {
     @DisplayName("1글자 미만, 10글자 초과의 글자를 활용한 RegionName 생성 - 실패")
     @ParameterizedTest
     @ValueSource(strings = {"", "10자이상의RegionName"})
-    void construct_nameLengthUnder1OrOver10_throwIllegalArgumentException(String regionName) {
+    void construct_NameLengthUnder1OrOver10_throwIllegalArgumentException(String regionName) {
         assertThatThrownBy(() -> new RegionName(regionName))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage(RegionName.INVALID_LENGTH_ERROR);
