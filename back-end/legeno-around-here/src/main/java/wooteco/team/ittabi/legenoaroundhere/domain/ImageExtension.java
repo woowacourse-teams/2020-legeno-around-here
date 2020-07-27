8f4b7d43b02bad830dade4517bee8cd17f4eac81
aegis <aegis1920@gmail.com>
2020-07-28 00:15:26 +0900

    feat: 이미지 확장자 도메인 및 테스트 작성
    - NotImageExtensionException 커스텀 예외 추가
    - 이미지 확장자 유효성 검사 기능 추가
    - test1.txt 이름을 test4.txt로 변경

---
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/ImageExtension.java
@@ -0,0 +1,39 @@
+package wooteco.team.ittabi.legenoaroundhere.domain;
+
+import java.util.Arrays;
+import org.springframework.web.multipart.MultipartFile;
+import wooteco.team.ittabi.legenoaroundhere.exception.NotImageExtensionException;
+
+public enum ImageExtension {
+    JPG(".jpg"),
+    JPEG(".jpeg"),
+    GIF(".gif"),
+    ICO(".ico"),
+    SVG(".svg"),
+    TIF(".tif"),
+    TIFF(".tiff"),
+    WEBP(".webp");
+
+    private final String extension;
+
+    ImageExtension(String extension) {
+        this.extension = extension;
+    }
+
+    public static void validateImageExtension(MultipartFile file) {
+        if (noneMatchImageExtension(file)) {
+            throw new NotImageExtensionException(file.getName() + "은/는 이미지 파일에 맞는 확장자가 아닙니다!");
+        }
+    }
+
+    private static boolean noneMatchImageExtension(MultipartFile file) {
+        return Arrays.stream(ImageExtension.values())
+            .noneMatch(imageExtension -> isSame(file, imageExtension.extension));
+    }
+
+    private static boolean isSame(MultipartFile file, String extension) {
+        return file.getName()
+            .toLowerCase()
+            .endsWith(extension);
+    }
+}
