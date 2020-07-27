720470eea8b664c818712d1901db26f115374351
junk-sound <wom2277@naver.com>
2020-07-27 23:05:50 +0900

    refac: BaseEntity에 Auditing 기능 추가

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/BaseEntity.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/BaseEntity.java
@@ -1,14 +1,17 @@
 package wooteco.team.ittabi.legenoaroundhere.domain;
 
 import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
     @Id
---
+++ back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/repository/PostRepositoryTest.java
@@ -0,0 +1,15 @@
+package wooteco.team.ittabi.legenoaroundhere.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class PostRepositoryTest {
+
+    @Test
+    void haha() {
+
+    }
+}
