45a2d333e01e72df6e917665ff423a8f06d85820
junk-sound <wom2277@naver.com>
2020-07-28 11:05:09 +0900

    feat: RegionRepository - save()

---
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/repository/RegionRepository.java
@@ -0,0 +1,8 @@
+package wooteco.team.ittabi.legenoaroundhere.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wooteco.team.ittabi.legenoaroundhere.domain.area.Region;
+
+public interface RegionRepository extends JpaRepository<Region, Long> {
+
+}
---
+++ back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/repository/RegionRepositoryTest.java
@@ -0,0 +1,39 @@
+package wooteco.team.ittabi.legenoaroundhere.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import wooteco.team.ittabi.legenoaroundhere.domain.area.Region;
+import wooteco.team.ittabi.legenoaroundhere.domain.area.RegionName;
+
+@DataJpaTest
+class RegionRepositoryTest {
+
+    @Autowired
+    private RegionRepository regionRepository;
+
+    @DisplayName("regionName을 활용한 Region 생성")
+    @ParameterizedTest
+    @ValueSource(strings = {"서울시", "서대문구", "홍제동"})
+    void save_validRegionName_Region(String regionName) {
+        Region region = new Region(regionName);
+        assertThat(region.getId()).isNull();
+        assertThat(region.getArea()).isNull();
+        assertThat(region.getName()).isEqualTo(new RegionName(regionName));
+        assertThat(region.getCreatedAt()).isNull();
+        assertThat(region.getModifiedAt()).isNull();
+
+        region = regionRepository.save(region);
+        assertThat(region.getId()).isNotNull();
+        assertThat(region.getArea()).isNull();
+        assertThat(region.getName()).isEqualTo(new RegionName(regionName));
+        assertThat(region.getCreatedAt()).isNotNull();
+        assertThat(region.getModifiedAt()).isNotNull();
+    }
+
+    //todo: findById()
+}
