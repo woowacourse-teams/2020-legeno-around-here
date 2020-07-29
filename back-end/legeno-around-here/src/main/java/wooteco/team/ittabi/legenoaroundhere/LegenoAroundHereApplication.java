37526e392fbac9bdc80a714860268b7cfd9f33f3
Geunwon Lim <wom2277@naver.com>
2020-07-29 09:07:39 +0900

    refactor: Jpa Auditing - BaseEntity에 Audit 기능 추가 [ISSUE-60] (#61)
    * refac: BaseEntity에 Auditing 기능 추가
    
    * test: PostRepository의 save()
    
    * refac: 코드에 optimize 적용

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/LegenoAroundHereApplication.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/LegenoAroundHereApplication.java
@@ -2,7 +2,9 @@ package wooteco.team.ittabi.legenoaroundhere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LegenoAroundHereApplication {
 
