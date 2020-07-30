487c31ea041f481df217337c19b0094058a12581
KimYelim <kiel0103@naver.com>
2020-07-30 20:42:09 +0900

    feat: 내 정보 조회 기능 구현 [ISSUE-8] (#66)
    * feat: 회원가입시 BCrypt Password Encoder 로 비밀번호 암호화 저장 구현
    
    - 프로젝트에 Spring Security 접목시킴
    - 패스워드를 암호화하면서
      패스워드 길이가 8 ~ 16자 사이인지 검증하는 로직이 사라졌음
    
    * feat: UserRole Enum 작성
    
    * feat: Jwt 토큰 생성기 구현
    
    * feat: 로그인 구현
    
    * refactor: 사용자 관련 entity 에 custom exception 적용
    
    * refactor: custom exception 추가적용 및 테스트 코드 개선
    
    * refactor: 전채코드 줄바꿈 리팩토링
    
    * refactor: User 객체 생성시의 UserRole 문자열상수 처리
    
    * refactor: User Role 리팩토링
    
    * refactor: 필요없는 주석 제거
    
    * refactor: 제이미, 빙봉 피드백 반영
    
    * feat: 내정보 조회 구현
    
    * refactor: 해결된 Todo 주석에서 Todo 를 지움

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/user/User.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/user/User.java
@@ -110,4 +110,8 @@ public class User implements UserDetails {
             ", password=" + password +
             '}';
     }
+
+    public String getNicknameByString() {
+        return getNickname().getNickname();
+    }
 }
