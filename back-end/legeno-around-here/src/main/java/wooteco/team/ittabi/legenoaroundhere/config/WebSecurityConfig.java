108c603ad0bc07c0e15942a6856f56754def7455
Hamliet <hamliet@hanyang.ac.kr>
2020-08-03 01:15:34 +0900

    fix: CORS 포트 번호 변경 (#97)
    긴급 이슈로 바로 Merge합니다.

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/config/WebSecurityConfig.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("http://13.209.62.31:3000");
+        configuration.addAllowedOrigin("http://13.209.62.31:80");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
@@ -56,6 +56,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .cors()
             .and()
             .addFilterBefore(new JwtAuthenticationFilter(jwtTokenDecoder),
-                UsernamePasswordAuthenticationFilter.class);;
+                UsernamePasswordAuthenticationFilter.class);
+        ;
     }
 }
