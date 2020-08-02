ba10913ed1e1581a2dc4871ac636383bd7f6ed49
junk-sound <wom2277@naver.com>
2020-08-03 02:22:09 +0900

    refac: initial_data.sql 파일에서 sql문을 대문자로 수정

--- back-end/legeno-around-here/src/main/resources/initial_data.sql
+++ back-end/legeno-around-here/src/main/resources/initial_data.sql
@@ -2,8 +2,8 @@
 -- raw_legal_area는 법적으로 주소를 분류한 테이블입니다.
 -- 애플리케이션 시작 시 raw_legal_area가 존재한다면 삭제 합니다.
 -- raw_legal_area 테이블을 생성합니다. legal_code는 법정동 코드, county는 시도명, city는 시군구명, town은 읍면동명, village는 동리명을 뜻합니다.
-drop table if exists raw_legal_area;
-create table raw_legal_area (
+DROP TABLE IF EXISTS raw_legal_area;
+CREATE TABLE raw_legal_area (
     id bigint auto_increment,
     legal_code varchar(30),
     county varchar(30),
@@ -25,57 +25,57 @@ LOAD DATA INFILE '/legeno_around_here/initial_data/raw_legal_area.csv'
 
 -- [2] 임시 테이블을 활용하여 지역 관련 초기 데이터 셋팅
 -- raw_legal_area 테이블을 활용하여 area 테이블에 데이터를 셋팅합니다.
-insert into area (dtype, id, created_at, code)
-select "Legal", null, current_time(), raw.legal_code
-from raw_legal_area as raw;
+INSERT INTO area (dtype, id, created_at, code)
+    SELECT "Legal", null, current_time(), raw.legal_code
+    FROM raw_legal_area as raw;
 
 -- raw_legal_area 테이블을 활용하여 region 테이블에 데이터를 셋팅합니다.
-insert ignore into region (name)
-select distinct county from raw_legal_area
-union
-select distinct city from raw_legal_area
-union
-select distinct town from raw_legal_area
-union
-select distinct village from raw_legal_area;
+INSERT IGNORE INTO region (name)
+    SELECT DISTINCT county from raw_legal_area
+    UNION
+    SELECT DISTINCT city from raw_legal_area
+    UNION
+    SELECT DISTINCT town from raw_legal_area
+    UNION
+    SELECT DISTINCT village from raw_legal_area;
 
 -- area, region, raw_legal_area 테이블을 활용하여 area 테이블과 region의 관계를 맺어줍니다. 즉 regional_relationship 테이블을 셋팅합니다.
-insert ignore into regional_relationship (area_id, region_id)
-    select prcessed_area_region.area_id, prcessed_area_region.region_id from
-        (select distinct area_id, region_id from
-            raw_legal_area  join
-            (select id as area_id, code from area) as area
-            on raw_legal_area.legal_code = area.code
-            join (select id as region_id, name from region) as region
-            on raw_legal_area.county = region.name
+INSERT IGNORE INTO regional_relationship (area_id, region_id, created_at)
+    SELECT prcessed_area_region.area_id, prcessed_area_region.region_id, current_time() FROM
+        (SELECT DISTINCT area_id, region_id FROM
+            raw_legal_area  JOIN
+            (SELECT id AS area_id, code FROM area) AS area
+            ON raw_legal_area.legal_code = area.code
+                            JOIN (SELECT id AS region_id, name FROM region) AS region
+                                 ON raw_legal_area.county = region.name
 
-        union
+         UNION
 
-        select distinct area_id, region_id from
-            raw_legal_area join
-            (select id as area_id, code from area) as area
-            on raw_legal_area.legal_code = area.code
-            join (select id as region_id, name from region) as region
-            on raw_legal_area.city = region.name
+         SELECT DISTINCT area_id, region_id FROM
+             raw_legal_area JOIN
+             (SELECT id AS area_id, code FROM area) AS area
+             ON raw_legal_area.legal_code = area.code
+                            JOIN (SELECT id AS region_id, name FROM region) AS region
+                                 ON raw_legal_area.city = region.name
 
-        union
+         UNION
 
-        select distinct area_id, region_id from
-            raw_legal_area join
-            (select id as area_id, code from area) as area
-            on raw_legal_area.legal_code = area.code
-            join (select id as region_id, name from region) as region
-            on raw_legal_area.town = region.name
+         SELECT DISTINCT area_id, region_id FROM
+             raw_legal_area JOIN
+             (SELECT id AS area_id, code FROM area) AS area
+             ON raw_legal_area.legal_code = area.code
+                            JOIN (SELECT id AS region_id, name FROM region) AS region
+                                 ON raw_legal_area.town = region.name
 
-        union
+         UNION
 
-        select area_id, region_id from
-            raw_legal_area join
-            (select id as area_id, code from area) as area
-            on raw_legal_area.legal_code = area.code
-            join (select id as region_id, name from region) as region
-            on raw_legal_area.village = region.name
-        ) as prcessed_area_region;
+         SELECT area_id, region_id FROM
+             raw_legal_area JOIN
+             (SELECT id AS area_id, code FROM area) AS area
+             ON raw_legal_area.legal_code = area.code
+                            JOIN (SELECT id AS region_id, name FROM region) AS region
+                                 ON raw_legal_area.village = region.name
+        ) AS prcessed_area_region;
 
 -- [3] 불필요한 임시 테이블 삭제
-drop table if exists raw_legal_area;
+DROP TABLE IF EXISTS raw_legal_area;
