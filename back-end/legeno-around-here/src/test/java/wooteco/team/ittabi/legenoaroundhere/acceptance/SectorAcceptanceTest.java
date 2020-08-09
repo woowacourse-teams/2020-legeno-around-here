6ee5960979a86c25f67e37568dab9d215ae4a077
jamie9504 <jamie9504@kakao.com>
2020-08-09 14:49:13 +0900

    feat: Sector Paging 조회 인수테스트 구현
    - PropertyReferenceException BadRequest 핸들링(Sort 대상 필드가 없는 경우 예외)
    - BindException BadRequest 핸들링(파라미터 바인딩 예외)

--- back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/acceptance/SectorAcceptanceTest.java
+++ back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/acceptance/SectorAcceptanceTest.java
@@ -12,9 +12,12 @@ import static wooteco.team.ittabi.legenoaroundhere.utils.constants.UserTestConst
 import static wooteco.team.ittabi.legenoaroundhere.utils.constants.UserTestConstants.TEST_SECTOR_NAME;
 
 import io.restassured.RestAssured;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -108,9 +111,9 @@ public class SectorAcceptanceTest {
         // 부문 전체 조회 + 사용하는 부문 전체 조회
         Long anotherId = createSector(accessToken, TEST_SECTOR_NAME, TEST_SECTOR_DESCRIPTION);
         List<AdminSectorResponse> adminSectorResponses
-            = findAllSector(accessToken, 0, 10, "id", "asc");
+            = findAllSector(accessToken);
         assertThat(adminSectorResponses).hasSize(2);
-        List<SectorResponse> sectorResponses = findAllInUseSector(accessToken, 0, 10, "id", "asc");
+        List<SectorResponse> sectorResponses = findAllInUseSector(accessToken);
         assertThat(sectorResponses).hasSize(2);
 
         // 부문 삭제
@@ -119,16 +122,118 @@ public class SectorAcceptanceTest {
         adminSectorResponse = findSector(accessToken, id);
         assertThat(adminSectorResponse.getState()).isEqualTo(SectorState.DELETED.name());
 
-        adminSectorResponses = findAllSector(accessToken, 0, 10, "id", "asc");
-        sectorResponses = findAllInUseSector(accessToken, 0, 10, "id", "asc");
+        adminSectorResponses = findAllSector(accessToken);
+        sectorResponses = findAllInUseSector(accessToken);
         assertThat(adminSectorResponses).hasSize(2);
         assertThat(sectorResponses).hasSize(1);
 
         deleteSector(accessToken, anotherId);
-        adminSectorResponses = findAllSector(accessToken, 0, 10, "id", "asc");
-        sectorResponses = findAllInUseSector(accessToken, 0, 10, "id", "asc");
+        adminSectorResponses = findAllSector(accessToken);
+        sectorResponses = findAllInUseSector(accessToken);
         assertThat(adminSectorResponses).hasSize(2);
         assertThat(sectorResponses).hasSize(0);
+    }
+
+    /**
+     * Feature: 부문 조회
+     * <p>
+     * Scenario: 부문을 페이징 조회한다.
+     * <p>
+     * Given 관리자가 로그인 되어있다. 부문 100개가 등록되어 있다.
+     * <p>
+     * When 부문 1Page 20Size를 정렬(기준:id 방향:오름차순) 조회한다. Then 1~20까지의 Sector가 조회된다.
+     * <p>
+     * When 부문 1Page 20Size를 정렬(기준:id 방향:내림차순) 조회한다. Then 100~81까지의 Sector가 조회된다.
+     * <p>
+     * When 부문 2Page 40Size를 정렬(기준:id 방향:오름차순) 조회한다. Then 21~40까지의 Sector가 조회된다.
+     * <p>
+     * When 부문 -1Page -1Size를 정렬(기준:id 방향:abc) 조회한다. Then 1의 Sector가 조회된다. (기본 값 : 1Page, 1Size,
+     * 방향:오름차순)
+     * <p>
+     * When 부문 1Page 20Size를 정렬(기준:test-id 방향:오름차순) 조회한다. Then BadRequest가 발생한다.
+     */
+    @DisplayName("부문 페이징 조회")
+    @Test
+    void pagingFindSector() {
+        // 관리자 로그인
+        createAdmin(TEST_ADMIN_EMAIL, TEST_ADMIN_NICKNAME, TEST_ADMIN_PASSWORD);
+        TokenResponse tokenResponse = login(TEST_ADMIN_EMAIL, TEST_ADMIN_PASSWORD);
+        String accessToken = tokenResponse.getAccessToken();
+
+        List<Long> ids = new ArrayList<>();
+        // 부문 100개 등록
+        for (int i = 0; i < 100; i++) {
+            ids.add(createSector(accessToken, TEST_SECTOR_NAME + i, TEST_SECTOR_DESCRIPTION));
+        }
+
+        // 부문 1Page 20Size를 정렬(기준:id 방향:오름차순) 조회
+        List<AdminSectorResponse> sectors
+            = findAllSector(accessToken, "page=1&size=20&sortedBy=id&direction=asc");
+        assertThat(sectors).hasSize(20);
+        List<Long> expectedIds = ids.subList(0, 20);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        // 부문 1Page 20Size를 정렬(기준:id 방향:내림차순) 조회
+        sectors = findAllSector(accessToken, "page=1&size=20&sortedBy=id&direction=desc");
+        assertThat(sectors).hasSize(20);
+        expectedIds = ids.subList(80, 100);
+        Collections.reverse(expectedIds);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        // 부문 2Page 20Size를 정렬(기준:id 방향:오름차순) 조회
+        sectors = findAllSector(accessToken, "page=2&size=20&sortedBy=id&direction=asc");
+        assertThat(sectors).hasSize(20);
+        expectedIds = ids.subList(20, 40);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        // Page, Size, Direction 오기입 조회 (자동 값 : 1Page, 1Size, 방향:오름차순)
+        sectors = findAllSector(accessToken, "page=-1&size=1&sortedBy=id&direction=asc");
+        assertThat(sectors).hasSize(1);
+        expectedIds = ids.subList(0, 1);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        sectors = findAllSector(accessToken, "page=1&size=-1&sortedBy=id&direction=asc");
+        assertThat(sectors).hasSize(1);
+        expectedIds = ids.subList(0, 1);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        sectors = findAllSector(accessToken, "page=1&size=51&sortedBy=id&direction=asc");
+        assertThat(sectors).hasSize(50);
+        expectedIds = ids.subList(0, 50);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        sectors = findAllSector(accessToken, "page=1&size=1&sortedBy=id&direction=abc");
+        assertThat(sectors).hasSize(1);
+        expectedIds = ids.subList(0, 1);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        sectors = findAllSector(accessToken, "size=1&sortedBy=id&direction=abc");
+        assertThat(sectors).hasSize(1);
+        expectedIds = ids.subList(0, 1);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        sectors = findAllSector(accessToken, "page=1&sortedBy=id&direction=abc");
+        assertThat(sectors).hasSize(10);
+        expectedIds = ids.subList(0, 10);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        sectors = findAllSector(accessToken, "page=1&size=1&sortedBy=id");
+        assertThat(sectors).hasSize(1);
+        expectedIds = ids.subList(0, 1);
+        assertThat(getSectorIds(sectors)).isEqualTo(expectedIds);
+
+        // 유효하지 않은 필드로 정렬
+        findAllSectorWithWrongParameter(accessToken, "page=ㄱ&size=1&sortedBy=id");
+        findAllSectorWithWrongParameter(accessToken, "page=1&size=ㄴ&sortedBy=id");
+        findAllSectorWithWrongParameter(accessToken, "page=1&size=20&sortedBy=ㄷ&direction=asc");
+    }
+
+    private List<Long> getSectorIds(List<AdminSectorResponse> sectors) {
+        return sectors.stream()
+            .peek(System.out::println)
+            .map(AdminSectorResponse::getId)
+            .peek(System.out::println)
+            .collect(Collectors.toList());
     }
 
     private String createAdmin(String email, String nickname, String password) {
@@ -214,8 +319,8 @@ public class SectorAcceptanceTest {
             .as(SectorResponse.class);
     }
 
-    private void updateSector(String accessToken, Long id,
-        String sectorName, String sectorDescription) {
+    private void updateSector(String accessToken, Long id, String sectorName,
+        String sectorDescription) {
         Map<String, String> params = new HashMap<>();
         params.put("name", sectorName);
         params.put("description", sectorDescription);
@@ -230,35 +335,54 @@ public class SectorAcceptanceTest {
             .statusCode(HttpStatus.OK.value());
     }
 
-    private List<AdminSectorResponse> findAllSector(String accessToken, int page, int size,
-        String sortedBy, String direction) {
+    private List<AdminSectorResponse> findAllSector(String accessToken) {
         return given()
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .header("X-AUTH-TOKEN", accessToken)
             .when()
-            .get("/admin/sectors?page=" + page + "&size=" + size + "&sortedBy=" + sortedBy
-                + "&direction=" + direction)
+            .get("/admin/sectors?page=1&size=10&sortedBy=id&direction=asc")
             .then()
             .statusCode(HttpStatus.OK.value())
-            .log()
-            .body()
+            .log().body()
             .extract()
             .jsonPath()
-            .getList("content");
+            .getList("content", AdminSectorResponse.class);
     }
 
-    private List<SectorResponse> findAllInUseSector(String accessToken, int page, int size,
-        String sortedBy, String direction) {
+    private List<AdminSectorResponse> findAllSector(String accessToken, String parameter) {
         return given()
             .accept(MediaType.APPLICATION_JSON_VALUE)
             .header("X-AUTH-TOKEN", accessToken)
             .when()
-            .get("/sectors?page=" + page + "&size=" + size + "&sortedBy=" + sortedBy
-                + "&direction=" + direction)
+            .get("/admin/sectors?" + parameter)
             .then()
             .statusCode(HttpStatus.OK.value())
-            .log()
-            .body()
+            .log().body()
+            .extract()
+            .jsonPath()
+            .getList("content", AdminSectorResponse.class);
+    }
+
+    private void findAllSectorWithWrongParameter(String accessToken, String parameter) {
+        given()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .header("X-AUTH-TOKEN", accessToken)
+            .when()
+            .get("/admin/sectors?" + parameter)
+            .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .log().all();
+    }
+
+    private List<SectorResponse> findAllInUseSector(String accessToken) {
+        return given()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .header("X-AUTH-TOKEN", accessToken)
+            .when()
+            .get("/sectors?page=1&size=10&sortedBy=id&direction=asc")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .log().body()
             .extract()
             .jsonPath()
             .getList("content", SectorResponse.class);
