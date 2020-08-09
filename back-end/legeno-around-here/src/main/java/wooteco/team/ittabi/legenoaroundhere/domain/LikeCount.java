6ef39714dfd62a781bd0d250c0c931019de59202
aegis <aegis1920@gmail.com>
2020-08-09 20:22:44 +0900

    feat: LikeCount 도메인 작성 및 테스트
    - LikeCount를 불변객체로 작성

---
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/LikeCount.java
@@ -0,0 +1,41 @@
+package wooteco.team.ittabi.legenoaroundhere.domain;
+
+import javax.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import wooteco.team.ittabi.legenoaroundhere.exception.WrongUserInputException;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+@Getter
+@EqualsAndHashCode
+@ToString
+public class LikeCount {
+
+    public static final int MIN_LIKE_COUNT = 0;
+    public static final int LIKE_COUNT_VALUE = 1;
+
+    private long likeCount;
+
+    public LikeCount(long likeCount) {
+        validate(likeCount);
+        this.likeCount = likeCount;
+    }
+
+    private void validate(long likeCount) {
+        if (likeCount < MIN_LIKE_COUNT) {
+            throw new WrongUserInputException("좋아요 수는 0 미만이 될 수 없습니다!");
+        }
+    }
+
+    public LikeCount addLikeCount() {
+        return new LikeCount(this.likeCount + LIKE_COUNT_VALUE);
+    }
+
+    public LikeCount minusLikeCount() {
+        return new LikeCount(this.likeCount - LIKE_COUNT_VALUE);
+    }
+}
