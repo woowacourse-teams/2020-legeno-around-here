d2483ae35d4d8d564f0462a2a00df05cc21f6019
aegis1920 <aegis1920@gmail.com>
2020-08-10 22:33:01 +0900

    refactor: 이미지 업로드 기능 리펙토링 (#126)
    * refactor: 이미지 업로드 기능 accessKey, secretKey 없이 할 수 있도록 수정
    
    * refactor: AWS S3 Mocking한 테스트 모두 실제 S3를 사용하도록 수정
    
    - @OneToMany 에 @OnDelete 속성 추가
    - S3와 관련된 서비스 테스트 모두 @SpringBootTest로 수정
    
    * refactor: S3Uploader convert() 로직 수정
    
    - createNewFile() -> transferTo()
    
    Co-authored-by: jamie9504 <jamie9504@kakao.com>

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/aws/S3Uploader.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/aws/S3Uploader.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.UUID;
@@ -12,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import wooteco.team.ittabi.legenoaroundhere.exception.FileIOException;
-import wooteco.team.ittabi.legenoaroundhere.exception.MultipartFileConvertException;
 
 @Slf4j
 @Component
@@ -21,11 +19,11 @@ public class S3Uploader {
     public static final String DIR_DELIMITER = "/";
     public static final String FILE_NAME_DELIMITER = "_";
 
-    private final AmazonS3 amazonS3;
+    private final AmazonS3 amazonS3Client;
     private final String bucket;
 
-    public S3Uploader(AmazonS3 amazonS3, String bucket) {
-        this.amazonS3 = amazonS3;
+    public S3Uploader(AmazonS3 amazonS3Client, String bucket) {
+        this.amazonS3Client = amazonS3Client;
         this.bucket = bucket;
     }
 
@@ -35,20 +33,24 @@ public class S3Uploader {
     }
 
     private String upload(File uploadFile, String dirName) {
-        String fileName = dirName
-            + DIR_DELIMITER
-            + UUID.randomUUID().toString()
-            + FILE_NAME_DELIMITER
-            + uploadFile.getName();
+        String fileName = generateFileName(uploadFile, dirName);
         String uploadImageUrl = putS3(uploadFile, fileName);
         removeNewFile(uploadFile);
         return uploadImageUrl;
     }
 
+    private String generateFileName(File uploadFile, String dirName) {
+        return dirName
+            + DIR_DELIMITER
+            + UUID.randomUUID().toString()
+            + FILE_NAME_DELIMITER
+            + uploadFile.getName();
+    }
+
     private String putS3(File uploadFile, String fileName) {
-        amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
             .withCannedAcl(CannedAccessControlList.PublicRead));
-        return amazonS3.getUrl(bucket, fileName).toString();
+        return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
     private void removeNewFile(File targetFile) {
@@ -61,18 +63,14 @@ public class S3Uploader {
 
     private File convert(MultipartFile multipartFile) {
         String originalFileName = Objects.requireNonNull(multipartFile.getOriginalFilename());
-        File convertFile = new File(originalFileName);
+        File file = new File(originalFileName);
         try {
-            if (convertFile.createNewFile()) {
-                FileOutputStream fileOutputStream = new FileOutputStream(convertFile);
-                fileOutputStream.write(multipartFile.getBytes());
-                return convertFile;
-            }
+            multipartFile.transferTo(file);
+            return file;
         } catch (IOException e) {
             log.error("File 읽기 실패, originalFileName = {}", originalFileName);
             throw new FileIOException("File을 읽고 쓰는데 실패했습니다!");
         }
-        log.debug("File 전환 실패, originalFileName = {}", originalFileName);
-        throw new MultipartFileConvertException("MultipartFile -> File로 전환이 실패했습니다.");
+
     }
 }
