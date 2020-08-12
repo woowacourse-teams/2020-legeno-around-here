83a75dad05e2cfa786296c3c796be521108c7176
jamie9504 <jamie9504@kakao.com>
2020-08-12 21:09:48 +0900

    feat: 수상 내역 조회 Mock 데이터 반환하는 컨트롤러 구현

--- back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/acceptance/AwardAcceptanceTest.java
+++ back-end/legeno-around-here/src/test/java/wooteco/team/ittabi/legenoaroundhere/acceptance/AwardAcceptanceTest.java
@@ -1,10 +1,15 @@
 package wooteco.team.ittabi.legenoaroundhere.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static wooteco.team.ittabi.legenoaroundhere.utils.constants.UserConstants.TEST_USER_EMAIL;
 import static wooteco.team.ittabi.legenoaroundhere.utils.constants.UserConstants.TEST_USER_NICKNAME;
 import static wooteco.team.ittabi.legenoaroundhere.utils.constants.UserConstants.TEST_USER_PASSWORD;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import wooteco.team.ittabi.legenoaroundhere.dto.AwardResponse;
 import wooteco.team.ittabi.legenoaroundhere.dto.TokenResponse;
 
@@ -18,15 +23,45 @@ public class AwardAcceptanceTest extends AcceptanceTest {
      * When 나의 수상받은 내역을 조회한다. Then 수상 내역이 조회된다.
      */
     @DisplayName("수상 내역 조회")
+    @Test
     void findAward() {
-        Long id = getIdFromUrl(createUser(TEST_USER_EMAIL, TEST_USER_NICKNAME, TEST_USER_PASSWORD));
+        Long userId = getIdFromUrl(
+            createUser(TEST_USER_EMAIL, TEST_USER_NICKNAME, TEST_USER_PASSWORD));
         TokenResponse token = login(TEST_USER_EMAIL, TEST_USER_PASSWORD);
         String accessToken = token.getAccessToken();
 
-        List<AwardResponse> awards = findAllAward(accessToken, id);
-        assertThat(awards).hasSize(10);
+        List<AwardResponse> awards = findAllAward(accessToken, userId);
+        assertThat(awards).hasSize(9);
 
         awards = findMyAward(accessToken);
-        assertThat(awards).hasSize(10);
+        assertThat(awards).hasSize(9);
+    }
+
+    private List<AwardResponse> findAllAward(String accessToken, Long userId) {
+        return given()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .header("X-AUTH-TOKEN", accessToken)
+            .when()
+            .get("/users/" + userId + "/awards")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract()
+            .jsonPath()
+            .getList(".", AwardResponse.class);
+    }
+
+    private List<AwardResponse> findMyAward(String accessToken) {
+        return given()
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+            .header("X-AUTH-TOKEN", accessToken)
+            .when()
+            .get("/awards/my")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract()
+            .jsonPath()
+            .getList(".", AwardResponse.class);
     }
 }
