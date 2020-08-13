c8ae23b65ff5c5b5e694d42694428415b0fe1c8c
김예림 <kiel0103@naver.com>
2020-08-13 17:11:40 +0900

    feat: mypage에 하단바디자인 적용

--- front-end/legeno-around-here/src/components/MyPage.js
+++ front-end/legeno-around-here/src/components/MyPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useMemo } from 'react';
-import axios from 'axios';
 
+import TopBar from './mypage/TopBar';
+import BottomBar from '../BottomBar';
 import { findMyInto } from './mypage/MyInfoFinder';
 import OutBox from './OutBox';
 import { getAccessTokenFromCookie } from '../util/TokenUtils';
@@ -14,8 +15,6 @@ import {
 } from './mypage/PrivacySection';
 import { AwardsSection, AwardSummary } from './mypage/AwardSection';
 import { NavSection, NavElement } from './mypage/LinksSection';
-
-import TopBar from './mypage/TopBar';
 
 function MyPage() {
   const [accessToken] = useState(getAccessTokenFromCookie());
@@ -31,30 +30,33 @@ function MyPage() {
   }, [accessToken]);
 
   return (
-    <OutBox>
-      <TopBar backButtonLink="/"></TopBar>
-      <TopSection>
-        <ProfilePhoto></ProfilePhoto>
-        <PrivacyBox>
-          <Nickname>{nickname}</Nickname>
-          <Email>{email}</Email>
-        </PrivacyBox>
-        <PrivacyEditBox>수정</PrivacyEditBox>
-      </TopSection>
-      <AwardsSection>
-        <AwardSummary awardName="TOP3" awardCount={1}></AwardSummary>
-        <AwardSummary awardName="TOP10" awardCount={0}></AwardSummary>
-        <AwardSummary awardName="TOP50" awardCount={12}></AwardSummary>
-      </AwardsSection>
-      <NavSection>
-        <NavElement linkTo="/">수상내역</NavElement>
-        <NavElement linkTo="/">작성글</NavElement>
-        <NavElement linkTo="/">작성 댓글</NavElement>
-        <NavElement linkTo="/">그외</NavElement>
-        <NavElement linkTo="/">뀨?</NavElement>
-        <NavElement linkTo="/">뀨뀨?</NavElement>
-      </NavSection>
-    </OutBox>
+    <>
+      <OutBox>
+        <TopBar backButtonLink="/"></TopBar>
+        <TopSection>
+          <ProfilePhoto></ProfilePhoto>
+          <PrivacyBox>
+            <Nickname>{nickname}</Nickname>
+            <Email>{email}</Email>
+          </PrivacyBox>
+          <PrivacyEditBox>수정</PrivacyEditBox>
+        </TopSection>
+        <AwardsSection>
+          <AwardSummary awardName="TOP3" awardCount={1}></AwardSummary>
+          <AwardSummary awardName="TOP10" awardCount={0}></AwardSummary>
+          <AwardSummary awardName="TOP50" awardCount={12}></AwardSummary>
+        </AwardsSection>
+        <NavSection>
+          <NavElement linkTo="/">수상내역</NavElement>
+          <NavElement linkTo="/">작성글</NavElement>
+          <NavElement linkTo="/">작성 댓글</NavElement>
+          <NavElement linkTo="/">그외</NavElement>
+          <NavElement linkTo="/">뀨?</NavElement>
+          <NavElement linkTo="/">뀨뀨?</NavElement>
+        </NavSection>
+      </OutBox>
+      <BottomBar></BottomBar>
+    </>
   );
 }
 
