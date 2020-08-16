53741fec3d6a768759e001c085f1dc5b7400e553
김예림 <kiel0103@naver.com>
2020-08-16 20:08:18 +0900

    refactor: 의미없는 코드 삭제

--- front-end/legeno-around-here/src/components/api/API.js
+++ front-end/legeno-around-here/src/components/api/API.js
@@ -24,7 +24,6 @@ export const getAllCurrentPosts = async (mainAreaId, page, accessToken) => {
     .catch((error) => {
       console.log(`최근 글을 가져올 수 없습니다! error : ${error}`);
       document.location.href = '/login';
-      return;
     });
   return response.data.content;
 };
