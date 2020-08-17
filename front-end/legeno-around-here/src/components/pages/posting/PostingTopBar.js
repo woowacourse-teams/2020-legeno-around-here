c0fc829eb8e79125058bd309d17b9ad74a5369e6
aegis <aegis1920@gmail.com>
2020-08-17 20:36:19 +0900

    refactor: 글쓰기 UI 수정

---
+++ front-end/legeno-around-here/src/components/pages/posting/PostingTopBar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import SendIcon from '@material-ui/icons/Send';
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(0),
+  },
+  title: {
+    display: 'block',
+  },
+  sectionDesktop: {
+    display: 'flex',
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  list: {
+    height: 400,
+    overflow: 'auto',
+  },
+}));
+
+export default function PrimarySearchAppBar() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="open drawer"
+              type="submit"
+              onClick={() => alert('전송!')}
+            >
+              <SendIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+}
