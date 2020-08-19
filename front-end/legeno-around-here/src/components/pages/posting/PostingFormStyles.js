4959ade63d379cbd6e67073e6f755eae6492c15e
KimYelim <kiel0103@naver.com>
2020-08-19 21:28:30 +0900

    feat: 글 작성시 부문 추가하기 기능 구현 [ISSUE-237] (#238)
    * feat: 글 등록 시 부문 설정하기 기능 구현
    
    * refactor: PostingForm 스타일 적용 코드를 다른 파일로 분리함
    
    * feat: 지역설정 UI 생성 (기능구현 X)
    
    * refactor: 사용되지 않는 setState 메서드 제거
    
    * refactor: 프로필사진 칸에 인터넷 상의 아무 사진 URL 넣어봄
    
    * refactor: 의미 없는 줄바꿈 제거 (#250)

---
+++ front-end/legeno-around-here/src/components/pages/posting/PostingFormStyles.js
@@ -0,0 +1,61 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(0),
+  },
+  title: {
+    display: 'block',
+  },
+  sectionDesktop: {
+    display: 'flex',
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  list: {
+    height: 400,
+    overflow: 'auto',
+  },
+  uploadPhoto: {
+    opacity: 0,
+    position: 'absolute',
+    zIndex: -1,
+    pointerEvents: 'none',
+  },
+  selectSectorButton: {
+    display: 'inlineBlock',
+    fontSize: '140%',
+    color: '#3366bb',
+  },
+  sector: {
+    display: 'inline',
+    borderRadius: 100,
+    backgroundColor: 'skyblue',
+    padding: '5px 8px 5px 8px',
+  },
+  selectAreaButton: {
+    display: 'inlineBlock',
+    fontSize: '140%',
+    color: '#3366bb',
+  },
+  area: {
+    display: 'inline',
+    borderRadius: 100,
+    backgroundColor: 'skyblue',
+    padding: '5px 8px 5px 8px',
+  }
+}));
+
+export default useStyles;
