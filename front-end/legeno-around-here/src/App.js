bd2070c53fd2d4cc3d5e178839ad388cd4157b17
aegis <aegis1920@gmail.com>
2020-08-21 20:05:23 +0900

    Merge branch 'develop' of https://github.com/woowacourse-teams/2020-legeno-around-here into feature/#267
    # Conflicts:
    #	front-end/legeno-around-here/src/App.js
    #	front-end/legeno-around-here/src/components/api/API.js
    #	front-end/legeno-around-here/src/components/pages/MyProfile.js
    #	front-end/legeno-around-here/src/components/pages/post/PostDetailPage.js
    #	front-end/legeno-around-here/src/components/pages/posting/PostingForm.js`

--- front-end/legeno-around-here/src/App.js
+++ front-end/legeno-around-here/src/App.js
@@ -5,8 +5,7 @@ import Login from './components/pages/LoginPage';
 import PostingPage from './components/pages/posting/PostingPage';
 import SectorPage from './components/pages/sector/SectorPage';
 import PostDetailPage from './components/pages/post/PostDetailPage';
-import MyProfileEditPage
-  from './components/pages/myProfileEdit/MyProfileEditPage';
+import MyProfileEditPage from './components/pages/myProfileEdit/MyProfileEditPage';
 import PostingUpdatePage from './components/pages/posting/PostingUpdatePage';
 import MyProfilePage from './components/pages/myProfile/MyProfilePage';
 import HomePage from './components/pages/HomePage';
