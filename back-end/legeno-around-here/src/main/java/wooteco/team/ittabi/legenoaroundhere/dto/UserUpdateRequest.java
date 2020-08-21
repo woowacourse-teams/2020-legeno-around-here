d802bb347f552552351b45787c041aee2486e367
jamie9504 <59511689+jamie9504@users.noreply.github.com>
2020-08-21 19:06:03 +0900

    refactor: 윈도우에서 파일 삭제 안되는 부분 개선 (#270)
    * feat: 빈 생성자 추가
    
    * refactor: fileOutputStream 닫아줌
    
    * refactor: file 삭제 방법 변경(윈도우 권한 관련)
    
    - 라흐 의견 반영
    
    긴급 머지 진행합니다. :)

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/dto/UserUpdateRequest.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/dto/UserUpdateRequest.java
@@ -1,11 +1,14 @@
 package wooteco.team.ittabi.legenoaroundhere.dto;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor
 @Getter
 @Setter
