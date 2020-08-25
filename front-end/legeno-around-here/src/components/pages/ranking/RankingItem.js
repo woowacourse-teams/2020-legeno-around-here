412a9df3c785b65b743449681c6ed2d3672a58bf
김예림 <kiel0103@naver.com>
2020-08-26 00:24:58 +0900

    refactor: 필요없는 import 제거

--- front-end/legeno-around-here/src/components/pages/ranking/RankingItem.js
+++ front-end/legeno-around-here/src/components/pages/ranking/RankingItem.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
