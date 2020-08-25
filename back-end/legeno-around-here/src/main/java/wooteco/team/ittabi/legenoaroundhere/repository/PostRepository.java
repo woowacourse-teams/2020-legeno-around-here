7bcc3340e84d4e67d7740a7d7addd139677ab750
jamie9504 <59511689+jamie9504@users.noreply.github.com>
2020-08-25 15:27:46 +0900

    refactor: Post 조회시 Native Query로 처리 (#297)

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/repository/PostRepository.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/repository/PostRepository.java
@@ -4,18 +4,43 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import wooteco.team.ittabi.legenoaroundhere.domain.area.Area;
+import org.springframework.data.jpa.repository.Query;
 import wooteco.team.ittabi.legenoaroundhere.domain.post.Post;
-import wooteco.team.ittabi.legenoaroundhere.domain.sector.Sector;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Page<Post> findAllBy(Pageable pageable);
 
-    Page<Post> findAllByAreaIn(Pageable pageable, List<Area> areas);
+    @Query(value = ""
+        + "SELECT * "
+        + "  FROM Post "
+        + " WHERE deleted_at IS NULL "
+        + "   AND area_id IN (SELECT id "
+        + "                     FROM area "
+        + "                    WHERE deleted_at IS NULL "
+        + "                      AND full_name LIKE (SELECT CONCAT(full_name,'%') "
+        + "                                            FROM area "
+        + "                                           WHERE deleted_at IS NULL "
+        + "                                             AND id = :areaId))", nativeQuery = true)
+    Page<Post> findAllByAreaId(Pageable pageable, Long areaId);
 
-    Page<Post> findAllBySectorIn(Pageable pageable, List<Sector> sectors);
+    @Query("SELECT p FROM Post p WHERE p.sector.id IN :sectorIds")
+    Page<Post> findAllBySectorIds(Pageable pageable, List<Long> sectorIds);
 
-    Page<Post> findAllByAreaInAndSectorIn(Pageable pageable, List<Area> areas,
-        List<Sector> sectors);
+    @Query(value = ""
+        + "SELECT * "
+        + "  FROM Post "
+        + " WHERE deleted_at IS NULL "
+        + "   AND area_id IN (SELECT id "
+        + "                     FROM area "
+        + "                    WHERE deleted_at IS NULL "
+        + "                      AND full_name LIKE (SELECT CONCAT(full_name,'%') "
+        + "                                            FROM area "
+        + "                                           WHERE deleted_at IS NULL "
+        + "                                             AND id = :areaId)) "
+        + "   AND sector_id IN (SELECT id "
+        + "                       FROM sector "
+        + "                      WHERE deleted_at IS NULL "
+        + "                        AND id IN :sectorIds)", nativeQuery = true)
+    Page<Post> findAllByAreaIdsAndSectorIds(Pageable pageable, Long areaId, List<Long> sectorIds);
 }
