d2e5bc3a40cd0b06b30d34a8cd568683b2d4ce16
jamie9504 <jamie9504@kakao.com>
2020-08-27 00:59:43 +0900

    refactor: 접근제어자 변경

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/util/AwardNameMaker.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/util/AwardNameMaker.java
@@ -7,7 +7,7 @@ import wooteco.team.ittabi.legenoaroundhere.domain.sector.Sector;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class AwardNameMaker {
 
-    protected static final String SECTOR_CREATOR_AWARD_NAME_SUFFIX = " 부문 창시자";
+    private static final String SECTOR_CREATOR_AWARD_NAME_SUFFIX = " 부문 창시자";
 
     public static String makeSectorCreatorAwardName(Sector sector) {
         return sector.getName() + SECTOR_CREATOR_AWARD_NAME_SUFFIX;
