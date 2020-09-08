b774b88bfbd532dbfd480c9c377138c350e33367
김예림 <kiel0103@naver.com>
2020-09-08 23:05:18 +0900

    feat: 글 상세 페이지에 글 삭제 버튼 추가

--- front-end/legeno-around-here/src/components/pages/post/PostDetail.js
+++ front-end/legeno-around-here/src/components/pages/post/PostDetail.js
@@ -13,6 +13,7 @@ import { convertDateFormat } from '../../../util/TimeUtils';
 import UpdatePostButton from './UpdatePostButton';
 import PostReportSection from './PostReportSection';
 import LinkWithoutStyle from '../../../util/LinkWithoutStyle';
+import DeletePostButton from './DeletePostButton'
 
 const PostDetail = ({ post, myInfo }) => {
   const accessToken = getAccessTokenFromCookie();
@@ -99,7 +100,10 @@ const PostDetail = ({ post, myInfo }) => {
         </Grid>
         <Grid container item xs={6} alignItems='flex-start' justify='flex-end' direction='row'>
           <Typography display='inline'>{convertDateFormat(post.createdAt)}</Typography>
-          <Typography display='inline'>{isMyPost && <UpdatePostButton post={post} />}</Typography>
+          <Typography display='inline'>
+            {isMyPost && <UpdatePostButton post={post} />}
+            {isMyPost && <DeletePostButton postId={post.id} accessToken={accessToken}/>}
+          </Typography>
           <PostReportSection post={post} myInfo={myInfo} />
         </Grid>
       </Grid>
