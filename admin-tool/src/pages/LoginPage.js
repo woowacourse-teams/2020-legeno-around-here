90a34457beb2f43d22e521d2cf9910a1ed9b5183
jamie9504 <jamie9504@kakao.com>
2020-09-17 00:49:38 +0900

    feat: Copyright 컴포넌트 분리

--- admin-tool/src/pages/LoginPage.js
+++ admin-tool/src/pages/LoginPage.js
@@ -6,7 +6,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
@@ -14,19 +13,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { isEmail } from '../util/Validation';
 import { useCookies } from 'react-cookie';
-
-const Copyright = () => {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://capzzang.co.kr">
-        ITTABI
-      </Link>{' '}
-      2020.08
-      {'.'}
-    </Typography>
-  );
-};
+import Copyright from '../components/Copyright';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
