2717f9be5cb5a86b87fa8b9f97e5ab37ee177fd9
jamie9504 <59511689+jamie9504@users.noreply.github.com>
2020-09-17 15:16:22 +0900

    feat: Admin Role을 가진 계정만 로그인 가능한 API 추가(For adminTool) (#448)
    * feat: 관리자만 로그인 가능한 API 추가
    
    * feat: 관리자 로그인 실패시, 권한 없음 - Forbidden 발생
    
    * refactor: 공백 제거

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/service/UserService.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/service/UserService.java
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 import wooteco.team.ittabi.legenoaroundhere.config.IAuthenticationFacade;
 import wooteco.team.ittabi.legenoaroundhere.domain.area.Area;
 import wooteco.team.ittabi.legenoaroundhere.domain.user.Email;
+import wooteco.team.ittabi.legenoaroundhere.domain.user.Role;
 import wooteco.team.ittabi.legenoaroundhere.domain.user.User;
 import wooteco.team.ittabi.legenoaroundhere.domain.user.UserImage;
 import wooteco.team.ittabi.legenoaroundhere.dto.LoginRequest;
@@ -28,6 +29,7 @@ import wooteco.team.ittabi.legenoaroundhere.dto.UserPasswordUpdateRequest;
 import wooteco.team.ittabi.legenoaroundhere.dto.UserResponse;
 import wooteco.team.ittabi.legenoaroundhere.dto.UserUpdateRequest;
 import wooteco.team.ittabi.legenoaroundhere.exception.AlreadyExistUserException;
+import wooteco.team.ittabi.legenoaroundhere.exception.NotAuthorizedException;
 import wooteco.team.ittabi.legenoaroundhere.exception.NotExistsException;
 import wooteco.team.ittabi.legenoaroundhere.exception.WrongUserInputException;
 import wooteco.team.ittabi.legenoaroundhere.infra.JwtTokenGenerator;
@@ -85,12 +87,33 @@ public class UserService implements UserDetailsService {
     }
 
     @Transactional
-    public TokenResponse login(LoginRequest loginRequest) {
-        User user = userRepository.findByEmail(new Email(loginRequest.getEmail()))
+    public TokenResponse loginAdmin(LoginRequest loginRequest) {
+        User user = findUserByEmail(loginRequest);
+        validateAdmin(user);
+        return getTokenResponse(loginRequest, user);
+    }
+
+    private User findUserByEmail(LoginRequest loginRequest) {
+        return userRepository.findByEmail(new Email(loginRequest.getEmail()))
             .orElseThrow(() -> new NotExistsException("가입되지 않은 회원입니다."));
+    }
+
+    private TokenResponse getTokenResponse(LoginRequest loginRequest, User user) {
         checkPassword(loginRequest, user);
         String token = jwtTokenGenerator.createToken(user.getUsername(), user.getRoles());
         return new TokenResponse(token);
+    }
+
+    private void validateAdmin(User user) {
+        if (user.hasNotRole(Role.ADMIN)) {
+            throw new NotAuthorizedException("관리자가 아닙니다.");
+        }
+    }
+
+    @Transactional
+    public TokenResponse login(LoginRequest loginRequest) {
+        User user = findUserByEmail(loginRequest);
+        return getTokenResponse(loginRequest, user);
     }
 
     private void checkPassword(LoginRequest loginRequest, User user) {
