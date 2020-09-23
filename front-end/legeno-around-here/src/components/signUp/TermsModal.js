783e188cbaf09585638cd5bec4f1beae4f205fd5
jamie9504 <59511689+jamie9504@users.noreply.github.com>
2020-09-24 05:51:27 +0900

    feat: 회원가입시 약관 동의 모달 구현 (#463)
    * feat: 로그인 화면, 회원가입 문구 Link 하이라이팅
    
    * feat: 회원 가입 화면, 로그인 문구 Link 하이라이팅
    
    * refactor: Copyright 컴포넌트 분리
    
    * feat: 모달 띄우기
    
    * feat: 모달과 페이지간의 체크박스 연동
    
    * refactor: 동일한 부분 같은 상수 사용
    
    * feat: 필수 약관 동의시에만 회원가입 가능하도록 validate
    
    * feat: 약관 본문 추가
    
    * feat: 약관 본문 수정
    
    * refactor: 불필요한 console log 제거
    
    * refactor: Modal TextField의 필요없는 label 속성 제거

---
+++ front-end/legeno-around-here/src/components/signUp/TermsModal.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Modal from '@material-ui/core/Modal';
+import Checkbox from '@material-ui/core/Checkbox';
+import { termsContent } from '../../constants/TermsContent';
+
+export const getModalStyle = () => {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+};
+
+export const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 874,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+const TermsModal = ({ open, closeModal, agree, setAgree }) => {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
+  const onChangeAgree = (event) => {
+    event.preventDefault();
+    const turnedAgree = !agree;
+    setAgree(turnedAgree);
+    if (turnedAgree) {
+      closeModal();
+    }
+  };
+
+  return (
+    <>
+      <Modal
+        open={open}
+        onClose={closeModal}
+        aria-labelledby='simple-modal-title'
+        aria-describedby='simple-modal-description'
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>우리동네캡짱 이용약관</h2>
+          <TextField
+            id='outlined-multiline-static'
+            multiline
+            rows={10}
+            defaultValue={termsContent}
+            variant='outlined'
+            fullWidth
+            InputProps={{
+              readOnly: true,
+            }}
+          />
+          <Checkbox
+            color='primary'
+            inputProps={{ 'aria-label': 'secondary checkbox' }}
+            checked={agree}
+            onChange={onChangeAgree}
+          />
+          우리동네캡짱 이용약관에 동의합니다.(필수)
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default TermsModal;
