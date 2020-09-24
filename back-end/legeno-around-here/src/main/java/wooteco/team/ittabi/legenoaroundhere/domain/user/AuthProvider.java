a09fabfd5b4bce52433244b44c0d72f885eea3f2
jamie9504 <jamie9504@kakao.com>
2020-09-24 11:26:24 +0900

    refactor: AuthProvider Enum name값 분리

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/user/AuthProvider.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/user/AuthProvider.java
@@ -3,18 +3,24 @@ package wooteco.team.ittabi.legenoaroundhere.domain.user;
 import java.util.Arrays;
 
 public enum AuthProvider {
-    LOCAL,
-    GOOGLE,
-    GITHUB;
+    LOCAL("local"),
+    GOOGLE("google"),
+    GITHUB("github");
+
+    private final String name;
+
+    AuthProvider(String name) {
+        this.name = name;
+    }
 
     public static AuthProvider of(String provider) {
         return Arrays.stream(AuthProvider.values())
-            .filter(authProvider -> authProvider.name().equalsIgnoreCase(provider))
+            .filter(authProvider -> authProvider.name.equalsIgnoreCase(provider))
             .findFirst()
             .orElseThrow(() -> new IllegalArgumentException("해당 하는 AuthProvider가 없습니다."));
     }
 
     public boolean isSame(String provider) {
-        return this.name().equalsIgnoreCase(provider);
+        return this.name.equalsIgnoreCase(provider);
     }
 }
