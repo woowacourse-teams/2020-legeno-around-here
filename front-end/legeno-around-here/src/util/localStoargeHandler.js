f6ecffa8419a8abe5f9a6ee516aeb640fd17b49a
aegis <aegis1920@gmail.com>
2020-09-24 16:01:23 +0900

    refactor: localStorageUtils 로 이름 변경 및 기본값을 '전체'로 변경

--- front-end/legeno-around-here/src/util/localStoargeHandler.js
+++
@@ -1,9 +0,0 @@
-export const getMainAreaName = () => {
-  localStorage.getItem('mainAreaName') || localStorage.setItem('mainAreaName', '서울특별시');
-  return localStorage.getItem('mainAreaName');
-};
-
-export const getMainAreaId = () => {
-  localStorage.getItem('mainAreaId') || localStorage.setItem('mainAreaId', '1');
-  return localStorage.getItem('mainAreaId');
-};
