598c3792d78a8b3f4c2a704314aced9324a5fbd3
김예림 <kiel0103@naver.com>
2020-10-09 17:29:23 +0900

    feat: 스케쥴러 exception catch 추가

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/scheduler/PostAwardCreateScheduler.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/scheduler/PostAwardCreateScheduler.java
@@ -2,10 +2,12 @@ package wooteco.team.ittabi.legenoaroundhere.scheduler;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import wooteco.team.ittabi.legenoaroundhere.service.award.PopularPostAwardService;
 
+@Slf4j
 @AllArgsConstructor
 @Component
 public class PostAwardCreateScheduler {
@@ -14,6 +16,10 @@ public class PostAwardCreateScheduler {
 
     @Scheduled(cron = "0 0 0 1 * *")    // cron : 0초 0시 0분 1일 *월 *요일 (매월 1일 0시 0분 0초)
     public void createPopularPostAwards() {
-        popularPostAwardService.createPopularPostAwards(LocalDateTime.now());
+        try {
+            popularPostAwardService.createPopularPostAwards(LocalDateTime.now());
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
     }
 }
