7f4a220e86cc0a731e4d04fe305d436377beaadf
김예림 <kiel0103@naver.com>
2020-10-16 21:07:13 +0900

    refactor: 닉네임 규칙 변경
    구글의 닉네임 규칙을 따르도록 수정
     - 띄어쓰기 허용
     - 글자수 1 ~ 30자
     - 구글에서 금지하는 특수문자 금지

--- back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/user/Nickname.java
+++ back-end/legeno-around-here/src/main/java/wooteco/team/ittabi/legenoaroundhere/domain/user/Nickname.java
@@ -1,5 +1,7 @@
 package wooteco.team.ittabi.legenoaroundhere.domain.user;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -19,7 +21,9 @@ import wooteco.team.ittabi.legenoaroundhere.exception.WrongUserInputException;
 @ToString
 public class Nickname {
 
-    private static final int MAX_LENGTH = 10;
+    private static final int MAX_LENGTH = 30;
+    private static final List<String> FORBIDDEN_SUBSTRINGS = Arrays.asList(
+        "&", "=", "_", "\'", "-", "+", ",", "<", ">", "..");
 
     @Column(nullable = false)
     private String nickname;
@@ -32,8 +36,8 @@ public class Nickname {
     private void validate(String nickname) {
         validateNull(nickname);
         validateEmpty(nickname);
-        validateSpace(nickname);
         validateLength(nickname);
+        validateContainForbiddenStrings(nickname);
     }
 
     private void validateNull(String nickname) {
@@ -48,15 +52,22 @@ public class Nickname {
         }
     }
 
-    private void validateSpace(String nickname) {
-        if (nickname.contains(" ")) {
-            throw new WrongUserInputException("닉네임은 공백을 포함할 수 없습니다.");
-        }
-    }
-
     private void validateLength(String nickname) {
         if (nickname.length() > MAX_LENGTH) {
             throw new WrongUserInputException("닉네임은 " + MAX_LENGTH + "글자 이하여야 합니다.");
+        }
+    }
+
+    private void validateContainForbiddenStrings(String nickname) {
+        for (String forbiddenSubstring : FORBIDDEN_SUBSTRINGS) {
+            validateContainForbiddenString(nickname, forbiddenSubstring);
+        }
+    }
+
+    private void validateContainForbiddenString(String nickname, String forbiddenSubstring) {
+        if (nickname.contains(forbiddenSubstring)) {
+            throw new WrongUserInputException(
+                "닉네임은 " + forbiddenSubstring + "을 포함할 수 없습니다.");
         }
     }
 }
